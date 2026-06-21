70499954fa67d626c3c56cd3228f2157aad06eca
Planète Game Arcade et la jungle d arcade <mrgamerdu84@gmail.com>
2026-06-21 14:43:48 +0200

    Update TaxiRadio.tsx

--- src/components/TaxiRadio.tsx
+++ src/components/TaxiRadio.tsx
@@ -5,7 +5,7 @@ import junkyCityEmpireAsset from "@/assets/junky_city_empire.mp3.asset.json";
 import rockMusic1 from "@/assets/alex-morgan-rock-rock-music-545492.mp3";
 import rockMusic2 from "@/assets/alex-morgan-rock-rock-music-545498.mp3";
 import rockMusic3 from "@/assets/nastelbom-rock-rock-music-513418.mp3";
-const ROCK_TRACKS = [
+const ROCK_TRACKS = [rockMusic1, rockMusic3];
   rockMusic1,
   rockMusic2,
   rockMusic3,
